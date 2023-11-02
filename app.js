ec736e63aabc7b7da801d924cb74a978b46472c1
Jared Flores <a353391@uach.mx>
2023-11-01 19:42:14 -0600

    Implementado JWT

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const { expressjwt } = require('express-jwt');
 const mongoose = require('mongoose');
+const JwtKey = "259e0d524f7f9bb980f3fa9bcd6f987b";
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -15,7 +17,7 @@ const membersRouter = require('./routes/members');
 const copiesRouter = require('./routes/copies');
 
 var app = express();
-//  mongodb://<dbUser>?:<dbPass>?@<url>:zport>/<dbName>
+//  mongodb://<dbUser>?:<dbPass>?@<url>:<port>/<dbName>
 const url = "mongodb://localhost:27017/video-club";
 mongoose.connect(url);
 
@@ -40,6 +42,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(expressjwt({secret:JwtKey, algorithms:['HS256']})
+   .unless({path:["/login/"]}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,55 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 function home (req, res, next) {
     res.render('index', { title : 'Express' });
 }
 
 function login(req, res, next) {
-    res.render('index', { title : 'Express' });
+    const email = req.body.email;
+    const password = req.body.password;
+    const JwtKey = "259e0d524f7f9bb980f3fa9bcd6f987b";
+
+    User.findOne({ "_email" : email })
+        .then(user => {
+            if (user) {
+                bcrypt.hash(password, user.salt, (err, hash) => {
+                    if (err) {
+                        res.status(403).json({
+                            message: "Usuario y contraseña incorrecto",
+                            obj: err
+                        });
+                    }
+
+                    if (hash === user.password) {
+                        res.status(200).json({
+                            message: "Login ok",
+                            // -> stateless: no se distingue entre los roles del usuario, por lo que no hay discriminacion
+                            //    por lo que se usa el payload del toke para guardarlo pero en el token viene esa informacion
+                            //    para discriminar
+                            // -> Definir el armado de un jwt valido, nos pide tres cosas
+                            //    objeto con la informacion, normalmente es el id para poder usarlo en el futuro
+                            obj: jwt.sign({ data:user.data, exp:Math.floor(Date.now() / 1000)+60 }, JwtKey)
+                        });
+                    } else {
+                        res.status(403).json({
+                            message: "Usuario y contraseña incorrecto",
+                            obj: null
+                        });
+                    }
+                });
+            } else {
+                res.status(403).json({
+                    message: "Usuario y contraseña incorrecto",
+                    obj: null
+                });
+            }
+        }).catch(ex => res.status(403).json({
+            message: "Usuario y contraseña incorrecta",
+            obj: ex
+        }));
 }
 
 module.exports = {
